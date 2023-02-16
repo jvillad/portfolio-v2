c24971fd2991757901eaf358434d1975204f27ed
jvillad <jyvillad@gmail.com>
2023-02-16 21:50:19 +1100

    fetch projects and display in list

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,17 +1,35 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
+interface IProject {
+  owner: string;
+  repo: string;
+  link: string;
+  description: string;
+}
+
 function Projects() {
-  const [projects, setProjects] = useState();
-  const baseUrl = 'https://api.github.com/users/jvillad/repos';
+  const [projects, setProjects] = useState<IProject[]>();
+
+  const baseUrl = 'https://gh-pinned-repos.egoist.dev/?username=jvillad';
   useEffect(() => {
     axios.get(baseUrl).then((response: any) => {
       setProjects(response.data);
     });
   }, []);
+
   return (
-    <section className="max-w-[1200px] m-auto">
+    <section className="max-w-[1200px] mx-auto px-[16px]">
       <div>Projects that I am proud of: </div>
+      <div>
+        {projects &&
+          projects.map((el) => (
+            <div key={el.repo}>
+              <div>{el.repo}</div>
+              <div>{el.description}</div>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }
