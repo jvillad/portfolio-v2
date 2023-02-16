c4adb26fcb50a6e02d2383078db0e50c11567f52
jvillad <jyvillad@gmail.com>
2023-02-16 21:48:27 +1100

    new component for contact section

---
+++ src/components/Contacts.tsx
@@ -0,0 +1,13 @@
+function Contacts() {
+  return (
+    <div className="max-w-[1200px] text-center px-[16px] pt-[100px]  mx-auto">
+      <p>Contact Me</p>
+      <p>
+        My inbox is always open for opportunities, suggestions and collaboration
+      </p>
+      <button type="button">Send me an email</button>
+    </div>
+  );
+}
+
+export default Contacts;
