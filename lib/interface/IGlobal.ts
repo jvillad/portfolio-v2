8e2a50c4b76dd76d71e5cb596ec9366faf78f261
jvillad <jyvillad@gmail.com>
2023-02-17 12:53:26 +1100

    added global interface

---
+++ lib/interface/IGlobal.ts
@@ -0,0 +1,25 @@
+export interface IPinnedProjects {
+  owner: string;
+  repo: string;
+  link: string;
+  description: string;
+  image: string;
+  language: string;
+}
+
+export interface ILangResponse1 {
+  lang1: string;
+  lang2: string;
+}
+export interface ILangResponse2 {
+  lang1: string;
+  lang2: string;
+  lang3: string;
+}
+
+export interface ILangResponse3 {
+  lang1: string;
+  lang2: string;
+  lang3: string;
+  lang4: string;
+}
