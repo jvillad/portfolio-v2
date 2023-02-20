c07b811a80de5a4c9b5cb5b993faa40c1279ffc3
jvillad <jyvillad@gmail.com>
2023-02-21 00:36:36 +1100

    styling: font size, vertical padding & font weight

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,11 +1,18 @@
 function Contact() {
   return (
-    <div className="max-w-[1200px] text-center px-[16px] pt-[150px] mx-auto pb-[150px]">
-      <p>Get In Touch</p>
+    <div className="max-w-[1200px] text-center px-[16px] pt-[150px] mx-auto pb-[150px] text-xl">
+      <p className="py-2">Get In Touch</p>
       <p>
         My inbox is always open for opportunities, suggestions and collaboration
       </p>
-      <button type="button">Send me an email</button>
+      <div className="py-4">
+        <button
+          type="button"
+          className="font-semibold py-2 bg-yellow-400 text-yellow-800 p-4 rounded-2xl"
+        >
+          Send me an email
+        </button>
+      </div>
     </div>
   );
 }
