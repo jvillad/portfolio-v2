295cfecc356561a26b6fbfc9bc90f1716ef9b8da
jvillad <jyvillad@gmail.com>
2023-02-23 21:08:43 +1100

    styling for name and title

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,15 +1,17 @@
 function Intro() {
   return (
-    <section className="max-w-[900px] mx-auto pt-[80px] pr-[16px] pb-[100px] pl-[16px] h-screen">
+    <section className="max-w-[900px] mx-auto pt-[140px] pr-[16px] pb-[100px] pl-[16px] h-screen">
       <p>
         <span className="text-2xl pb-2">
           <span className="text-yellow-800">Yo! 👋🏼</span> my name is
         </span>
-        <span className="block text-[50px] md:text-[64px]">
+        <span className="block text-[50px] md:text-[64px] font-semibold">
           John Villademosa
         </span>
       </p>
-      <p className="pb-6 text-2xl pt-2">An aspiring Software Developer. </p>
+      <p className="pb-6 text-2xl pt-2">
+        An aspiring <span className="text-yellow-800">Software Developer.</span>
+      </p>
       <p className=" text-xl md:text-2xl leading-relaxed">
         Focusing my learning on ASP.Net Core MVC, PostgresSQL and TypeScript.
         Also, spending some time building web applications in React and NextJS.
