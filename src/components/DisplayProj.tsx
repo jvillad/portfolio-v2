991a6116c52d99522545a40fa4317ef70b4820b1
jvillad <jyvillad@gmail.com>
2023-02-24 14:46:45 +1100

    styling to align and center icon and language

--- src/components/DisplayProj.tsx
+++ src/components/DisplayProj.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import getRepo from '../../lib/helper/getProjectLang';
 import { ILangResponse1, TProject } from '../../lib/interface/IGlobal';
+import SelectIcon from './SelectIcon';
 
 function DisplayProj({ proj }: TProject) {
   const [languages, setLanguages] = useState<ILangResponse1>();
@@ -20,19 +21,23 @@ function DisplayProj({ proj }: TProject) {
   return (
     <div
       key={project.repo}
-      className="bg-gray-700 text-white text-center rounded p-1"
+      className="bg-gray-800 text-white text-center rounded-md p-[2px]"
     >
       <a href={project.link} target="_blank" rel="noreferrer">
         <img
           src={project.image}
           alt={project.repo}
-          className="w-[440px] h-[240px]"
+          className="w-[440px] h-[240px] min-w-[310px] rounded-t-md"
         />
-        <div className="text-xs p-1">
+        <div className="text-xs font-light p-1">
           {langKeys?.map((key) => (
-            <p key={key} className="text-gray-200 text-[10px]">
-              {key}
-            </p>
+            <div
+              key={key}
+              className="flex items-center w-[80px] max-w-full justify-between mx-auto py-[2px]"
+            >
+              <SelectIcon progLang={key} />
+              <p className="text-gray-200 text-[10px]">{key}</p>
+            </div>
           ))}
         </div>
       </a>
