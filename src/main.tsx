ff3764ccfe3a629864027756a883e5c3b0ea74d5
jvillad <jyvillad@gmail.com>
2023-07-04 23:19:11 +1000

    wrapping the whole app with the provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './apollo';
 import App from './App';
 import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={apolloClient}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
