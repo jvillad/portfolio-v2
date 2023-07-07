a66af44de53b1f840aacf40e6b3362eec8ab18a1
jvillad <jyvillad@gmail.com>
2023-07-07 19:17:21 +1000

    added a featured project

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,21 +1,33 @@
 function Intro() {
   return (
-    <section className="max-w-[900px] mx-auto pt-[70px] pr-[16px] pl-[16px] mb-24 sm:mb-56">
-      <p>
-        <span className="text-xl pb-2">
-          <span className="text-yellow-800">👋🏼 Yo! </span> my name is
-        </span>
-        <span className="block text-[50px] md:text-[64px] font-semibold">
-          John Villademosa
-        </span>
-      </p>
-      <p className="pb-6 text-2xl pt-2">
-        An aspiring <span className="text-yellow-800">Software Developer.</span>
-      </p>
-      <p className="text-md md:text-xl leading-relaxed">
-        Focusing my learning on ASP.Net Core, PostgreSQL and TypeScript. Also,
-        spending some time building web applications using NextJS and Angular.
-      </p>
+    <section className="flex flex-col sm:flex-row mb-24 sm:mb-56 items-center max-w-[900px] mt-16 px-[16px]">
+      <div className="w-32 md:w-64 h-40 md:h-80 mx-auto sm:mx-0 sm:mr-5 md:mr-8 flex items-center">
+        <img
+          src="https://res.cloudinary.com/supremevillad/image/upload/c_scale,w_2316/v1688704464/358735218_1205490856809870_3616068741508629111_n_cb7lwq.jpg"
+          alt="the best web developer that made this portfolio"
+          className="rounded-md"
+        />
+      </div>
+      <div className="my-4 max-w-xl">
+        <div className="mt-6">
+          <p>
+            <span className="text-xl">
+              <span className="text-rose-600">👋🏼 Yo! </span> my name is
+            </span>
+            <span className="block text-[50px] md:text-[54px] font-semibold">
+              John Villademosa
+            </span>
+          </p>
+          <p className="pb-4 text-2xl">An aspiring Software Developer.</p>
+        </div>
+        <div className="mb-8">
+          <p className="text-md md:text-lg leading-relaxed">
+            Focusing my learning on ASP.Net Core, PostgreSQL and TypeScript.
+            Also, spending some time building web applications using NextJS and
+            Angular.
+          </p>
+        </div>
+      </div>
     </section>
   );
 }
--- src/components/MainProjects.tsx
+++ src/components/MainProjects.tsx
@@ -1,9 +1,11 @@
 function MainProjects() {
   return (
     <section className="max-w-[1200px] mx-auto px-[16px] my-10">
-      <div className="flex flex-col lg:flex-row items-center">
+      <div className="flex flex-col lg:flex-row items-center my-20 sm:my-40 sm:flex-row gap-5">
         <div className="lg:w-1/2 lg:mr-4">
-          <p>Feature Project</p>
+          <p className="font-extralight text-xs text-rose-600">
+            Featured Project
+          </p>
           <h2 className="text-2xl font-bold mb-2">
             <a
               href="https://beanscenecafe.azurewebsites.net/"
@@ -13,7 +15,7 @@ function MainProjects() {
               Cafe Beanscene Reservation
             </a>
           </h2>
-          <p className="mb-10">
+          <p className="mb-10 p-1">
             A web application that provides secure user authentication and
             authorization features. Users can create accounts and make
             reservations while staff members have the ability to manage
@@ -21,7 +23,7 @@ function MainProjects() {
             includes a manager dashboard.
           </p>
           <p>Tech Stack</p>
-          <ul className="flex justify-between font-light text-sm mx-5">
+          <ul className="flex justify-between font-light text-sm">
             <li>ASP.Net Core MVC</li>
             <li>EF Core</li>
             <li>MS SQL</li>
@@ -32,14 +34,60 @@ function MainProjects() {
           <img
             src="https://res.cloudinary.com/supremevillad/image/upload/c_scale,q_auto,w_1079/v1688618432/BeansceneReservation_r1yo9o.png"
             alt="Beanscene Reservation Screenshot"
-            className="w-full"
+            className="w-full opacity-75 transition-opacity duration-300 hover:opacity-100 rounded-md"
           />
           <div className="absolute -bottom-9 left-0 right-0 text-white flex justify-center py-2">
             <a
               href="https://beanscenecafe.azurewebsites.net/"
               target="_blank"
               rel="noreferrer"
-              className="bg-yellow-400 p-3 text-yellow-800 rounded-lg hover:bg-rose-600 hover:text-rose-50 shadow-xl"
+              className="bg-yellow-400 p-2 text-yellow-800 rounded-lg hover:bg-rose-600 hover:text-rose-50 shadow-xl"
+            >
+              Visit Site
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col lg:flex-row-reverse items-center sm:flex-row-reverse gap-5">
+        <div className="lg:w-1/2 lg:ml-4 sm:text-right">
+          <p className="font-extralight text-xs text-rose-600">
+            Featured Project
+          </p>
+          <h2 className="text-2xl font-bold mb-2">
+            <a
+              href="https://beanscenecafe.azurewebsites.net/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Simple Todo List + Firestore
+            </a>
+          </h2>
+          <p className="mb-10 p-1">
+            A simple and user-friendly to-do list web app that offers secure
+            Google account login, ensuring your tasks are saved uniquely to your
+            user account. Powered by Firebase for reliable and secure cloud
+            storage of all your tasks.
+          </p>
+          <p>Tech Stack</p>
+          <ul className="flex justify-between font-light text-sm sm:text-right">
+            <li>Vite</li>
+            <li>React</li>
+            <li>TailwindCSS</li>
+            <li>Firebase</li>
+          </ul>
+        </div>
+        <div className="lg:w-1/2 relative flex justify-center items-center">
+          <img
+            src="https://res.cloudinary.com/supremevillad/image/upload/c_scale,q_auto:best,w_1187/v1688650080/Screenshot_2023-07-06_at_10.40.28_pm_ywnio9.png"
+            alt="Beanscene Reservation Screenshot"
+            className="w-full opacity-75 transition-opacity duration-300 hover:opacity-100 rounded-md"
+          />
+          <div className="absolute -bottom-9 left-0 right-0 text-white flex justify-center py-2">
+            <a
+              href="https://easychecklist.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-yellow-400 p-2 text-yellow-800 rounded-lg hover:bg-rose-600 hover:text-rose-50 shadow-xl"
             >
               Visit Site
             </a>
