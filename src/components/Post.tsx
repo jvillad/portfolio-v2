98a2a96b3d4a32b8524b065c4f5ba0419f7bde98
jvillad <john.y.villademosa@gmail.com>
2023-09-29 23:21:50 +1000

    fetching blocks + rendering every block

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,33 +1,28 @@
-import ReactMarkdown from 'react-markdown';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import { renderBlock } from './notion/NotionRenderer';
 
 function Post() {
-  const { pageId } = useParams();
-  const { data, isLoading } = useQuery('blog-posts', async () => {
-    const response = await fetch(
-      `https://jv-notion-server.onrender.com/api/blog-posts/${pageId}`
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const blogs = await response.json();
-    return blogs;
-  });
-
+  const { slug } = useParams();
+  const url = `https://notion-server.onrender.com/v1/api/blog-posts/${slug}`;
+  const queryKey = ['blog-post', slug];
+  const queryFn = () => fetch(url).then((res) => res.json());
+  const { data, isLoading } = useQuery(queryKey, queryFn);
   if (isLoading) {
-    return (
-      <div className="min-h-screen w-full flex items-center justify-center">
-        <p className="animate-pulse">Hold on, getting them posts</p>
-      </div>
-    );
+    return <div>Loading...</div>;
   }
 
   return (
     <div className="max-w-[700px] mx-auto min-h-screen">
-      <article className="prose lg:prose-xl my-10">
-        <ReactMarkdown className="text-sm">{data.parent}</ReactMarkdown>
+      <article
+        className="prose lg:prose-xl my-10"
+        suppressHydrationWarning={true}
+      >
+        {data.map((block: any) => (
+          <div key={block.id} className="text-sm">
+            {renderBlock(block)}
+          </div>
+        ))}
       </article>
     </div>
   );
