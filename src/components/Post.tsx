2ecd339be50d3f2c8cd3695b936eaf916764f028
jvillad <john.y.villademosa@gmail.com>
2023-09-30 17:39:54 +1000

    caching

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -6,8 +6,22 @@ function Post() {
   const { slug } = useParams();
   const url = `https://notion-server.onrender.com/v1/api/blog-posts/${slug}`;
   const queryKey = ['blog-post', slug];
-  const queryFn = () => fetch(url).then((res) => res.json());
-  const { data, isLoading } = useQuery(queryKey, queryFn);
+
+  const { data, isLoading } = useQuery(
+    queryKey,
+    async () => {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      const blogPostData = await response.json();
+      return blogPostData;
+    },
+    {
+      cacheTime: 5 * 60 * 1000, // Cache data for 5 minutes
+    }
+  );
+
   if (isLoading) {
     return (
       <div className="min-h-screen w-full flex items-center justify-center">
